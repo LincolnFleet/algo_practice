00e29baada18565634705e4f967461f709fd73a6
Travis Martin <travisemerymartin@gmail.com>
2021-05-30 17:39:00 -0400

    swift syntax

--- fizzbuzz/fizzbuzz.swift
+++ fizzbuzz/fizzbuzz.swift
@@ -1,6 +1,14 @@
 // shebang??
 let number = [1,2,3,4,5,6,7,8,9,10]
 
-for num in number {
-  print("\(num) \(num%3 || "fizz")\(num%5 || "buzz")")
+for i in 0...100 {
+  if i % 3 == 0 && i % 5 == 0 {
+    print("fizzbuzz")
+  } else if  i % 3 == 0 {
+    print("fizz")
+  } else if i % 5 == 0 {
+    print("buzz")
+  } else {
+    print("\(i)")
+  }
 }
